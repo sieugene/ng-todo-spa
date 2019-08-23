9add230ee609b8206be59e18fb7949a0d0244716
sieugene <gagarin126rus@mail.ru>
2019-08-23 17:10:44 +0300

    basic_lesson

---
+++ src/app/todos/todos.component.ts
@@ -0,0 +1,35 @@
+import { TodosService } from './../shared/todos.service';
+import { Component, OnInit,} from '@angular/core';
+import { delay } from 'rxjs/operators';
+
+@Component({
+  selector: 'app-todos',
+  templateUrl: './todos.component.html',
+  styleUrls: ['./todos.component.scss']
+})
+export class TodosComponent implements OnInit {
+
+  private loading: boolean = true;
+  private searchString = ''
+  
+  constructor(private todosService: TodosService) { }
+
+  ngOnInit() {
+    this.todosService.fetchTodos()
+    .pipe(delay(500))
+    .subscribe(
+      () => {
+        this.loading = false;
+      }
+    );
+    
+  }
+
+  onChange(id: number){
+    this.todosService.onToogle(id)
+  }
+  removeTodo(id: number){
+    this.todosService.removeTodo(id)
+  }
+
+}
